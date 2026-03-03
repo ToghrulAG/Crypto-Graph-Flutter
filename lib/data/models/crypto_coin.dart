a1e5f9ba118acb0992cb07028ed74e0c1aee2e5d
Toghrul Aghali <toghrul@Toghruls-MacBook-Pro.local>
2026-03-03 20:43:51 +0300

    created simple models for crypto coins

---
+++ lib/data/models/crypto_coin.dart
@@ -0,0 +1,22 @@
+class CryptoCoin {
+  final String symbol;
+  final String price;
+  final String changePercent;
+
+  CryptoCoin({
+    required this.symbol,
+    required this.price,
+    required this.changePercent
+  });
+
+  factory CryptoCoin.fromJson(Map<String, dynamic> json) {
+    return CryptoCoin(
+      symbol : json['symbol'] as String,
+      price: json['price'] as String,
+      changePercent: json['changePercent'] as String
+    );
+  }
+  String get baseSymbol => symbol.replaceAll('USDT', '');
+
+  String get imageUrl => 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1x/32/color/${baseSymbol.toLowerCase()}.png';
+}
