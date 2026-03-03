2693502ff426603052e12af53ea5ee8f803b6218
Toghrul Aghali <toghrul@Toghruls-MacBook-Pro.local>
2026-03-03 20:46:27 +0300

    added data repository

---
+++ lib/data/network/api_client.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  final Dio dio;
+
+  ApiClient()
+    : dio = Dio(
+        BaseOptions(
+          baseUrl: 'https://api.binance.com/api/v3',
+          connectTimeout: const Duration(seconds: 5),
+          receiveTimeout: const Duration(seconds: 3),
+        ),
+      );
+
+  Future<List<dynamic>> getTickers() async {
+    try {
+      final response = await dio.get('/ticker/24hr');
+      return response.data;
+    } catch (e) {
+      throw Exception('Failed to load tickers: $e');
+    }
+  }
+}
---
+++ lib/data/repository/crypto_repository.dart
@@ -0,0 +1,30 @@
+import '../models/crypto_coin.dart';
+import '../network/api_client.dart';
+
+class CryptoRepository {
+  final ApiClient apiClient;
+
+  CryptoRepository({required this.apiClient});
+
+  List<CryptoCoin> _cachedCoins = [];
+
+  Future<List<CryptoCoin>> getCoins({int page = 1, int limit = 20}) async {
+
+    if(_cachedCoins.isEmpty) {
+      final rawData = await apiClient.getTickers();
+
+      _cachedCoins = rawData
+          .map((json) => CryptoCoin.fromJson(json as Map<String, dynamic>))
+          .where((coin) => coin.symbol.endsWith('USDT')) // Оставляем только пары к доллару
+          .toList();
+    }
+    final startIndex = (page - 1) * limit;
+
+    if(startIndex >= _cachedCoins.length) {
+      return [];
+    }
+    
+    return _cachedCoins.skip(startIndex).take(limit).toList();
+
+  }
+}
