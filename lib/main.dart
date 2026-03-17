0e3b57f465b0612aa130c84878e95125e690ee1a
ToghrulAG <agalitogrul14@gmail.com>
2026-03-17 11:08:17 +0300

    disabled device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
-import 'package:crypto_graph/features/crypto_list/presentation/crypto_list_screen.dart';
+import 'package:crypto_graph/features/crypto_list/screens/crypto_list_screen/crypto_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'data/network/api_client.dart';
 import 'data/repository/crypto_repository.dart';
 import 'features/crypto_list/bloc/crypto_list_bloc.dart';
 import 'features/crypto_list/bloc/crypto_list_event.dart';
+import 'package:device_preview/device_preview.dart'; //
 
 void main() {
 
   final apiClient = ApiClient();
   final cryptoRepository = CryptoRepository(apiClient: apiClient);
 
-  runApp(MyApp(repository: cryptoRepository));
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(repository: cryptoRepository),
+      enabled: false,
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,7 +28,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+
+    return ScreenUtilInit(
+
+      builder: (context, child) {
+        return MaterialApp(
       title: 'Crypto Monitor',
       theme: ThemeData.dark(), 
       
@@ -36,6 +47,8 @@ class MyApp extends StatelessWidget {
             child: CryptoListScreen()),
           ),
         ),
+    );
+      },
     );
   }
 }
